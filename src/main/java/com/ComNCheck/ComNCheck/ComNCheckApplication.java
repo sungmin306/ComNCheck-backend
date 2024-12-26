5b71bc5ec044649758624822f1500e5af89337a7
sungmin306 <another0306@naver.com>
2024-12-26 17:21:34 +0900

    Feat: start project

---
+++ src/main/java/com/ComNCheck/ComNCheck/ComNCheckApplication.java
@@ -0,0 +1,14 @@
+package com.ComNCheck.ComNCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComNCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComNCheckApplication.class, args);
+
+	}
+
+}
