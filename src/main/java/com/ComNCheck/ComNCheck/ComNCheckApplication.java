18a271bb3338a50a0aaca9b83ca0ca48e82a7ef8
sungmin306 <another0306@naver.com>
2024-12-27 17:41:17 +0900

    Feat: JWT ì시큐리티 ê¸ DTO, 토큰 생성 로직 개발 ãapplication.yaml 생성 이후 env 파일을 통한 환í환경변수 제어 ãMember 도메인 생성 이후 해당 패도메인에 토큰 DTO 및 토큰생성 코드  ê개발

--- src/main/java/com/ComNCheck/ComNCheck/ComNCheckApplication.java
+++ src/main/java/com/ComNCheck/ComNCheck/ComNCheckApplication.java
@@ -1,5 +1,6 @@
 package com.ComNCheck.ComNCheck;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ComNCheckApplication {
 
 	public static void main(String[] args) {
+
+		//Load .env file
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("H2_DB_URL", dotenv.get("H2_DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(ComNCheckApplication.class, args);
 
 	}
