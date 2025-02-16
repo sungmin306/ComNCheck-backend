e9889f338d6f6a91755cb68c06648f1a0396591c
sungmin306 <another0306@naver.com>
2025-02-16 22:35:28 +0900

    Refactor: 배포된 프론트 url 추가

--- src/main/java/com/ComNCheck/ComNCheck/domain/security/config/CorsMvcConfig.java
+++ src/main/java/com/ComNCheck/ComNCheck/domain/security/config/CorsMvcConfig.java
@@ -11,7 +11,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://localhost:3000"
-                ); // "https://com-n-check.vercel.app"
+                .allowedOrigins("http://localhost:3000",
+                        "https://com-n-check.vercel.app"
+                );
     }
 }
